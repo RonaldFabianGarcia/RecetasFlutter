0dba9e530b1066dfc5fa43e1335431909f63b5e9
RonalFabianGarcia <ronaldi_99@hotmail.com>
2019-07-18 01:36:51 -0500

    Deficion de Arquitectura

---
+++ recipes/lib/model/recipe_model.dart
@@ -0,0 +1,7 @@
+class Recipe {
+  Recipe({this.uid,this.recipe,this.image,this.name});
+  String uid;
+  String recipe;
+  String name;
+  String image;
+}
---
+++ recipes/lib/model/user_model.dart
@@ -0,0 +1,17 @@
+class Usuario {
+  Usuario({this.uid,
+          this.foto,
+          this.ciudad,
+          this.email,
+          this.nombre
+  });
+
+  String uid;
+  String foto;
+  String nombre;
+  String ciudad;
+  String email;
+  String password;
+  String telefono;
+  String direccion;
+}
