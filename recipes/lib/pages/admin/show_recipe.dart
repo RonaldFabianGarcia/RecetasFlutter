ded99a37aaa3a08b2ae48768c53cfbfc9bb79340
Ronald Fabian Garcia <ronaldi_99@hotmail.com>
2019-07-25 17:05:29 -0500

    Inicio Video 7

--- recipes/lib/pages/admin/show_recipe.dart
+++ recipes/lib/pages/admin/show_recipe.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:recipes/auth/auth.dart';
 class CommonThings {
   static Size size;
 }
@@ -12,12 +13,27 @@ String name;
 String phone;
 
 class InicioPage extends StatefulWidget {
-  final String 
+  final String id;
+  final BaseAuth auth;
+  final VoidCallback onSignedOut;
+  InicioPage({this.auth,this.onSignedOut,this.id});
+  
   @override
   _InicioPageState createState() => _InicioPageState();
 }
 
 class _InicioPageState extends State<InicioPage> {
+  String userID;
+  @override
+  void initState() {
+    setState(() {
+     Auth().currentUser().then((onValue){
+       userID = onValue;
+       print("User id $userID");
+     }); 
+    });        
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Container(
